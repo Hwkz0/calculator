054376b9bf75aba1126afbda63d1b7555ae07927
Hwkz0 <eduardddrimus@gmail.com>
2023-11-28 20:38:03 +0200

    Configured layout and colors

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -20,6 +20,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             //output area
             Expanded(
               child: SingleChildScrollView(
+                reverse: true,
                 child: Container(
                   alignment: Alignment.bottomRight,
                   padding: const EdgeInsets.all(16),
@@ -39,7 +40,9 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               children: Btn.buttonValues
                   .map(
                     (value) => SizedBox(
-                      width: screenSize.width / 4,
+                      width: value == Btn.n0
+                          ? screenSize.width / 2
+                          : (screenSize.width / 4),
                       height: screenSize.width / 5,
                       child: buildButton(value),
                     ),
@@ -53,6 +56,46 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   }
 
   Widget buildButton(value) {
-    return Text(value);
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Material(
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(100),
+          borderSide: const BorderSide(
+            color: Colors.white24,
+            width: 0.5,
+          ),
+        ),
+        child: InkWell(
+          onTap: () {},
+          child: Center(
+            child: Text(
+              value,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Color getBtnColor(value) {
+    return [Btn.del, Btn.clr].contains(value)
+        ? Colors.blueGrey
+        : [
+            Btn.per,
+            Btn.multiply,
+            Btn.add,
+            Btn.subtract,
+            Btn.divide,
+            Btn.calculate
+          ].contains(value)
+            ? Colors.orange
+            : Colors.black87;
   }
 }
